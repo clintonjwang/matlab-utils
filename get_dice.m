651d66b694ac703af99d6f3fa1af45e107b2511c
Clinton Wang <clintonjwang@gmail.com>
2018-02-10 22:07:56 -0500

    merge code for DICE, mask subtraction

---
+++ get_dice.m
@@ -0,0 +1,35 @@
+function dice = get_dice()
+% Try to detect masks automatically, otherwise prompt to select them
+
+    f=dir(fullfile('.', '*.ids'));
+    if length(f) == 2
+        path1 = [f(1).folder, '\', f(1).name];
+        path2 = [f(2).folder, '\', f(2).name];
+    else
+        [fname,outpath] = uigetfile('*.ids', 'Select first segmentation');
+        path1 = [outpath,fname];
+
+        [fname,outpath] = uigetfile('*.ids', 'Select second segmentation');
+        path2 = [outpath,fname];
+    end
+
+    % Try to automatically detect .ics files corresponding to one of them
+    fileID = fopen([path1(1:end-4), '.ics']);
+    if fileID == -1
+        [fname,outpath] = uigetfile('*.ics', ['Select the ics file corresponding to ', path1]);
+        fileID = fopen([outpath,fname]);
+    end
+
+    A = fscanf(fileID,'%c');
+    ics_text = strsplit(A(regexp(A, '\nlayout\tsizes\t8\t')+length('layout\tsizes\t8') :...
+                regexp(A, '\nlayout\tcoordinates')-2));
+    dims = cellfun(@str2num, ics_text);
+
+    mask1 = get_mask(path1, dims(1), dims(2), dims(3));
+    mask2 = get_mask(path2, dims(1), dims(2), dims(3));
+
+    A1 = sum(sum(sum(mask1)));
+    A2 = sum(sum(sum(mask2)));
+    overlap = sum(sum(sum(mask1 & mask2)));
+    dice = 2 * overlap / (A1+A2);
+end
--- get_mask.m
+++ get_mask.m
@@ -1,23 +1,23 @@
 function [mask] = get_mask(fpath)
 
-fileID = fopen([fpath,'.ics']);
-A = fscanf(fileID,'%c');
-D = sscanf(A(regexp(A, 'sizes\t')+8:regexp(A, 'layout\tcoordinates')-3), '%d');
-N1 = D(1);
-N2 = D(2);
-N3 = D(3);
+    fileID = fopen([fpath,'.ics']);
+    A = fscanf(fileID,'%c');
+    D = sscanf(A(regexp(A, 'sizes\t')+8:regexp(A, 'layout\tcoordinates')-3), '%d');
+    N1 = D(1);
+    N2 = D(2);
+    N3 = D(3);
 
-fileID = fopen([fpath,'.ids']);
-A = fread(fileID);
-ind = find(A);
-[i, j, k]=ind2sub([N2,N1,N3],ind);
-fclose('all'); 
+    fileID = fopen([fpath,'.ids']);
+    A = fread(fileID);
+    ind = find(A);
+    [i, j, k]=ind2sub([N2,N1,N3],ind);
+    fclose('all'); 
 
-mask = zeros(N1,N2,N3);
-for count=1:length(i) 
-    mask(i(count),j(count),k(count))=1; 
-end
+    mask = zeros(N1,N2,N3);
+    for count=1:length(i) 
+        mask(i(count),j(count),k(count))=1; 
+    end
 
-mask = transpose_mask_slices(mask, 'r');
+    mask = transpose_mask_slices(mask, 'r');
 
-return 
+end 
